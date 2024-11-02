606941b7b5913caf37f182932aaa2fe942eb3617
kirthiSaiT <kirthisai251@gmail.com>
2024-11-02 12:16:32 +0530

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const express = require("express");
-const socket = require("socket");
+const socket = require("socket.io");
 const http = require("http");
-const {chess} = require("chess");
+const { Chess } = require("chess.js");
+const path = require("path");
+const { title } = require("process");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "W"; 
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index",{title:"Chess Game"});
+});
+
+io.on("connection",function(socket){
+    console.log(connected);
+})
+
+server.listen(3000, function() {
+    console.log("Server listening on port 3000");
+});
